0060fdf9e66381a6cf54d8bb03cc349d0402cfe1
uditbh123 <uditbhattarai847@gmail.com>
2026-02-27 21:55:52 +0200

    updated test_login.py with positive and multiple negative scenarios

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,18 +2,25 @@ from pages.login_page import LoginPage
 from utils.screenshot import take_screenshot
 import logging
 
-def test_valid_login(page):
-    logging.info("Navigating to SauceDemo website")
-    page.goto("https://www.saucedemo.com")
+def test_invalid_login(page):
+    login_page = LoginPage(page)
+    login_page.login("invalid_user", "wrong_password")
 
-    login = LoginPage(page)
-    logging.info("Performing login action")
-    login.login(username="standard_user", password="secret_sauce")
+    error_message = login_page.get_error_message
+    assert "Username and password do not match" in error_message
 
-    try:
-        assert "inventory" in page.url
-        logging.info("Login successful")
-    except AssertionError:
-        logging.error("Login failed, taking screenshot")
-        take_screenshot(page, "valid_login_failed")
-        raise
+def test_locked_user_login(page):
+    login_page = LoginPage(page)
+    login_page.login("locked_out_user", "secret_sauce")
+
+    error_message = login_page.get_error_message()
+    assert "Sorry, this user has been locked out" in error_message
+
+def test_empty_credentials(page):
+    login_page = LoginPage(page)
+    login_page.login("", "")
+
+    error_message = login_page.get_error_message()
+    assert "Username is required" in error_message
+
+    
